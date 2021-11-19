3a6875f872d899ae686012af5c56c615c8310f8c
Sam Tischaefer <samtisch@live.com>
2021-11-18 20:21:46 -0800

    Set ping port to 0 and echo port to 1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,8 +12,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants { //TODO: Get correct numbers
-    public static int UltraSensorPingChannel = -1;
-    public static int UltraSensorEchoChannel = -1;
+public final class Constants { 
+
+    //TODO: Double check these
+    public static int UltraSensorPingChannel = 0;
+    public static int UltraSensorEchoChannel = 1;
 
 }
